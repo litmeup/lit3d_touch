a9e3fda85ce4631024a86222d7a9fd4913b2f9bb
litmeup <iulia.fedyakina@gmail.com>
2021-04-22 00:08:56 +0500

    Three.js sectors

--- sectors/main.mjs
+++ sectors/main.mjs
@@ -1,11 +1,39 @@
-export default function sectorCoords(alpha, R, n) {
+export function sectorCoords(alpha, R, n) {
     let coords = [];
-    alpha =  alpha * Math.PI / 180;
+    let alphaRad =  alpha * Math.PI / 180;
     for(let i=0; i < (n+1); i++) {
-        let gamma = -1 * 90 * Math.PI / 180 - alpha * (0.5 - i / n);
+        let gamma = -1 * 90 * Math.PI / 180 - alphaRad * (0.5 - i / n);
         let x = Math.cos(gamma) * R;
         let y = Math.sin(gamma) * R;
-        coords[i] = [x, y];
+        coords[i] = [x, y, gamma];
     }
     return coords;
+}
+
+export function sectorTransforms(alpha, R, n) {
+    //TransformedVector = TranslationMatrix * RotationMatrix
+    let secCoords = sectorCoords(alpha, R, n);
+    let transforms = [];
+    for(let i=0; i < n; i++) {
+        let translationMatrix = [[1,0,0,secCoords[i][1]], [0,1,0,0], [0,0,1,secCoords[i][0]], [0,0,0,1]];
+        let gamma = secCoords[i][2];
+        let rotationMatrix = [[Math.cos(gamma), 0, Math.sin(gamma), 0], [0, 1, 0, 0], [-1*Math.sin(gamma), 0, Math.cos(gamma), 0], [0,0,0,1]];
+        let resultingMatrix = math.multiply(translationMatrix, rotationMatrix);
+        let three4x4Matrix = [];
+        for(let j = 0; j < 4; j++)
+        {
+            three4x4Matrix = three4x4Matrix.concat(...resultingMatrix[j]);
+        }
+        console.log(three4x4Matrix);
+        transforms.push(three4x4Matrix);
+    }
+    return transforms;
+}
+
+export function boxSize(alpha, R, n, aspect) {
+    let alphaRad =  alpha * Math.PI / 180;
+    let beta = alphaRad / n;
+    let W = 2 * R * Math.sin(beta / 2);
+    let H = W * aspect;
+    return {w: W, h: H};
 }
